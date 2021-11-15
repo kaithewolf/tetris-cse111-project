1a325a39e2b1a93a6916a56082d50da07b9f29ca
kaithewolf <xtremekai27@gmail.com>
2021-11-14 22:10:48 -0800

    database type restructure, insert sample data

--- create-db.sql
+++ create-db.sql
@@ -1,27 +1,35 @@
 .headers on
+drop table MultiplayerGames;
+drop table SinglePlayer;
+drop table MapLeaderboard;
+drop table Leaderboards;
+drop table PlayersinMultiplayerGames;
+drop table PlayersInMap;
+
 --put your code here
 create table Users(
  username varchar(255) primary key);
 
 create table Leaderboards(
- rank int primary key,
+ modeRank int,
  username varchar(255) ,
- record int ,
+ record float ,
  timeSet varchar(255),
- gameType int );
+ gameType int,
+ primary key (modeRank, gameType));
 
 create table SinglePlayer(
  recordID int primary key,
  username varchar(255) ,
- gameTime varchar(255),
- date_played datetime,
+ gameTime float,
+ date_played date,
  piecesDropped int,
  gameType int );
 
 create table Multiplayer(
  recordID int primary key,
  username varchar(255) ,
- gameTime varchar(255),
+ gameTime float,
  attackSent int,
  piecesDropped int,
  b2b int);
@@ -30,29 +38,31 @@ create table MultiplayerGames(
  MatchID varchar(255),
  roomID varchar(255),
  gameMode int,
- datePlayed datetime);
+ datePlayed date);
 
 create table PlayersinMultiplayerGames(
  MatchID varchar(255),
- listOfPlayers varchar(255));
+ listOfPlayers varchar(255),
+ MatchRecord int);
 
 create table customMap(
  MapID int primary key,
  MapName varchar(255),
  createdBy varchar(255),
- difficulty int,
+ difficulty float,
  finishCondition int );
 
 create table PlayersInMap(
- MapID int primary key,
- player varchar(255) );
+ MapID int,
+ player varchar(255));
 
 create table MapLeaderboard(
- MapID int primary key,
- rank int ,
+ MapID int,
+ mapRank int,
  username varchar(255) ,
- record varchar(255),
- dateSet datetime);
+ record float,
+ dateSet date,
+primary key (MapID, mapRank));
 
 create table GameType(
  g_id int primary key,
---
+++ insert-sample.sql
@@ -0,0 +1,108 @@
+.headers on
+--put your code here
+insert into Users(username)
+values('keitar431'),
+('jimothynoob'),
+('hi0000'),
+('upDog'),
+('guuuuhhhh');
+
+insert into Leaderboards(modeRank,username,record,timeSet,gameType)
+values
+    (1, 'jimothynoob', 30.67, '2021-11-30', 1),
+    (2, 'keitar431', 45.99, '2021-12-10', 1),
+    (3, 'hi0000', 65.99, '2021-10-30', 1),
+    (4, 'upDog', 95.34, '2020-09-12', 1),
+    (1, 'guuuuhhhh', 56.78, '2021-10-31', 4);
+
+insert into SinglePlayer(
+ recordID,
+ username,
+ gameTime,
+ date_played,
+ piecesDropped,
+ gameType)
+ values
+     (13459, 'jimothynoob', 30.67, '2021-11-30', 100, 1),
+     (15684, 'keitar431', 45.99, '2021-12-10', 105, 1),
+     (17803, 'hi0000', 65.99, '2021-10-30', 109, 1),
+     (13950, 'upDog', 95.34, '2020-09-12', 103, 1),
+     (13850, 'guuuuhhhh', 56.78, '2021-10-31', 40, 4);
+
+insert into Multiplayer(
+ recordID,
+ username,
+ gameTime,
+ attackSent,
+ piecesDropped,
+ b2b )
+ values
+     (23840, 'jimothynoob', 93.31, 99, 130, 16),
+     (34920, 'keitar431', 93.10, 60, 95, 5),
+     (34293, 'hi0000', 34.50, 5, 30, 2),
+     (49604, 'upDog', 35.20, 28, 20, 0);
+
+insert into MultiplayerGames(
+ MatchID  ,
+ roomID  ,
+ gameMode ,
+ datePlayed )
+ values
+     ('EGDFH', 'SJED120', 1, '2021-12-10'),
+     ('TYULK', 'SLEOD130', 1, '2021-11-08');
+
+insert into PlayersinMultiplayerGames(
+ MatchID  ,
+ listOfPlayers  ,
+ MatchRecord )
+ values
+     ('EGDFH', 'jimothynoob', 23840),
+     ('EGDFH', 'keitar431', 34920),
+     ('TYULK', 'hi0000', 34293),
+     ('TYULK', 'upDog', 49604);
+
+insert into customMap(
+ MapID   ,
+ MapName  ,
+ createdBy  ,
+ difficulty ,
+ finishCondition  )
+ values
+     (74830, 'T-Spins', 'guuuuhhhh', 33.2, 1),
+     (12345, 'Scuffed PC', 'jimothynoob', 55.34, 2),
+     (23456, 'testing s z spin', 'guuuuhhhh', 67.7, 2);
+
+insert into PlayersInMap(
+ MapID ,
+ player)
+ values
+     (74830, 'hi0000'),
+     (74830, 'jimothynoob'),
+     (12345, 'jimothynoob'),
+     (23456, 'guuuuhhhh');
+
+insert into MapLeaderboard(
+ MapID,
+ mapRank,
+ username,
+ record,
+ dateSet)
+ values
+     (74830, 1, 'hi0000',  0.56, '2021-12-05'),
+     (74830, 2, 'jimothynoob', 0.65, '2021-10-15'),
+     (12345, 1, 'jimothynoob', 0.35, '2021-10-16'),
+     (23456, 1, 'guuuuhhhh', 2.5, '2021-11-10');
+
+insert into GameType(g_id, g_name)
+ values
+     (1,'sprint'),
+     (3,'cheese'),
+     (4,'survival'),
+     (5,'ultra');
+
+insert into FinishCondition(f_id   , f_name  )
+values
+    (1, 'clear_blocks'),
+    (2, 'pc');
+
+.headers off
