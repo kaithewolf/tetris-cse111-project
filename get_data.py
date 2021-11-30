0f3cfa7417da9e68499a50b5662ab22ad3e406e1
kaithewolf <xtremekai27@gmail.com>
2021-11-30 06:30:25 -0800

    parse leaderboard function

--- get_data.py
+++ get_data.py
@@ -47,7 +47,7 @@ def executemany(_conn, string, tup):
     except Error as e:
         print(e)
 
-def print_select(_conn, string, number):
+def print_select(_conn, string):
     try:
         cur = _conn.cursor()
         cur.execute(string)
@@ -59,6 +59,20 @@ def print_select(_conn, string, number):
     except Error as e:
         print(e)
 
+def parse_leaderboard(parsed):
+        count = 0
+        parsed_list = []
+        for result in parsed:
+            name = result["name"]
+            dateSet = result["ts"].split()[0]
+            rank = result["pos"]
+            record = result["game"]
+            parsed_list.append((rank, name, record, dateSet))
+            count += 1
+            if count > 99:
+                break
+        return parsed_list
+        
 def main():
     database = r"tetris.sqlite"
 
@@ -68,29 +82,20 @@ def main():
         #Single Player Leaderboards, top 100 of each
         sprint_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/1?mode=1&offset=0")
         sprint_parsed = json.loads(sprint_results.text.encode('utf8'))
-
-        count = 0
-
-        for result in sprint_parsed:
-            name = result["name"]
-            dateSet = result["ts"].split()[0]
-            rank = result["pos"]
-            record = result["game"]
-            print(name)
-            count += 1
-            if count > 99:
-                break
-
+        sprint_list = parse_leaderboard(sprint_parsed)
+        
         cheese_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/3?mode=1&offset=0")
         cheese_parsed = json.loads(cheese_results.text.encode('utf8'))
-        count = 0
-        for result in cheese_parsed:
-            print(result)
-            count += 1
-            if count > 99:
-                break
-        #survival_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/4?mode=1&offset=0")
-        #ultra_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/5?mode=1&offset=0")
+        cheese_list = parse_leaderboard(cheese_parsed)
+        
+        survival_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/4?mode=1&offset=0")
+        survival_parsed = json.loads(survival_results.text.encode('utf8'))
+        survival_list = parse_leaderboard(survival_parsed)
+
+        ultra_results = requests.get("http://jstris.jezevec10.com/api/leaderboard/5?mode=1&offset=0")
+        ultra_parsed = json.loads(ultra_results.text.encode('utf8'))
+        ultra_list = parse_leaderboard(ultra_parsed)
+
 
 
     closeConnection(conn, database)
