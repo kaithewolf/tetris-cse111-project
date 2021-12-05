0b67972dbad7be7fc5f97212a98375a26ab540b9
kaithewolf <xtremekai27@gmail.com>
2021-12-04 20:19:51 -0800

    added other container

--- app/scripts/Menu.gd
+++ app/scripts/Menu.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 onready var selection = preload("res://scene/MenuSelection.tscn")
-onready var control = get_node("ScrollContainer/Control")
-
+onready var control_obj = get_node("ScrollContainer/Control")
+onready var control_node = Control.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,8 +12,9 @@ func populate_menu(list):
 	for tuple in list:
 		var select = selection.instance()
 		select.set_text(tuple)
-		control.add_child(select)
+		control_obj.add_child(select)
 		select.set_owner(get_tree().get_edited_scene_root())
+	control_obj.add_child(control_node)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- app/scripts/app.gd
+++ app/scripts/app.gd
@@ -1,26 +1,40 @@
 extends Node2D
-var Sqlite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
-var db_path = "res://tetris.db"
-var db
+onready var Sqlite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+onready var db_path = "res://tetris.db"
+onready var db 
 onready var user_menu = get_node("Menu")
+onready var other_menu = get_node("Menu2")
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+func select_from_table(command):
+	print(db.query(command))
+	
+	var list = []
+	for i in range(0, db.query_result.size()):
+		list.append(db.query_result[i])
+	
+	return list.duplicate()
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	db = Sqlite.new()
 	db.path = db_path
 	db.open_db()
-	var command = "select * from Users;"
-	print(db.query(command))
 	
-	var list = []
-	for i in range(0, db.query_result.size()):
-		list.append(db.query_result[i]["username"])
+	var command = """
+	select * from Users;
+	"""
+	var username_list = select_from_table(command)
+	user_menu.populate_menu(username_list)
 	
-	user_menu.populate_menu(list)
+	command = """
+	select * from Sprint_Leaderboard;
+	"""
+	var sprint_list = select_from_table(command)
+	other_menu.populate_menu(sprint_list)
+	other_menu.get_node("ScrollContainer").scroll_horizontal = true
 	pass # Replace with function body.
 
 
