40802614df8895f8ee57fc86e1453797baa10f95
kaithewolf <xtremekai27@gmail.com>
2021-12-04 19:05:21 -0800

    added username menu function

--- app/scripts/app.gd
+++ app/scripts/app.gd
@@ -1,5 +1,7 @@
 extends Node2D
-
+var Sqlite = preload("res://addons/godot-sqlite/bin/gdsqlite.gdns")
+var db_path = "res://tetris.db"
+var db
 onready var user_menu = get_node("Menu")
 
 # Declare member variables here. Examples:
@@ -8,7 +10,16 @@ onready var user_menu = get_node("Menu")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var list = ["kaithewolf", "nateqwq", "uwuwuwu", "guhhh"]
+	db = Sqlite.new()
+	db.path = db_path
+	db.open_db()
+	var command = "select * from Users;"
+	print(db.query(command))
+	
+	var list = []
+	for i in range(0, db.query_result.size()):
+		list.append(db.query_result[i]["username"])
+	
 	user_menu.populate_menu(list)
 	pass # Replace with function body.
 
