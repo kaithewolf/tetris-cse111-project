326b6d77377d82e19cff06f1fbd671972c0ca707
kaithewolf <xtremekai27@gmail.com>
2021-12-07 19:06:18 -0800

    labels on points, points scale to max size of graph

--- app/scripts/Graph.gd
+++ app/scripts/Graph.gd
@@ -1,7 +1,12 @@
 extends Node2D
 onready var point = preload("res://scene/Point.tscn")
+onready var y_axis = get_node("y_axis")
+onready var x_axis = get_node("x_axis")
 var x_offset = 150
 var y_offset = -50
+#max range of graph, to be mapped onto
+var x_axis_end = 700
+var y_axis_end = 400
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,11 +17,21 @@ func _ready():
 	graph_points()
 
 func graph_points():
-	var x_list = [0, 50, 100]
-	var y_list = [0, 50, 100]
+	var x_list = [0, 400, 900]
+	var y_list = [0, 400, 900]
+	var xmin = x_list.min()
+	var xmax = x_list.max()
+	var ymin = y_list.min()
+	var ymax = y_list.max()
+	
+	#scale each point onto the graph
 	for i in range(len(x_list)):
 		var new_point = point.instance()
-		new_point.position = Vector2(x_list[i]+x_offset, -y_list[i]+y_offset)
+		
+		new_point.position = Vector2(float(x_list[i])/xmax*x_axis_end + x_offset, -float(y_list[i])/ymax*y_axis_end + y_offset)
 		self.add_child(new_point)
 		new_point.set_owner(self)
+		var x_str = x_axis.text+": "+str(x_list[i])
+		var y_str = y_axis.text+": "+str(y_list[i])
+		new_point.set_data(x_str, y_str)
 #	pass
--- app/scripts/Point.gd
+++ app/scripts/Point.gd
@@ -1,5 +1,6 @@
 extends Node2D
 onready var fill = get_node("Filled Circle")
+onready var data_label = get_node("Data")
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,19 +9,23 @@ onready var fill = get_node("Filled Circle")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Data.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
+func set_data(x_str:String, y_str:String):
+	data_label.text = x_str+"\n"+y_str
 
 func _on_Node2D_mouse_entered():
 	fill.visible = true
+	data_label.visible = true
 
 
 
 
 func _on_Node2D_mouse_exited():
 	fill.visible = false
+	data_label.visible = false
+	
